5db736a97bd3159a49a1ba665e1f946f76dcc592
Aryan Sureja <aryansureja2095@gmail.com>
2025-09-12 14:45:51 +0530

    header

---
+++ src/app/common/header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <header className="w-full z-50">
+    {/* Top Banner */}
+    <div className="bg-[#132961] text-white h-[38px] max-w-9xl mx-auto px-4 sm:px-8 lg:px-20 flex items-center justify-center sm:justify-start">
+      <p className="text-sm text-center sm:text-left">
+        Buy a Brand License | Sell your Business | Expand Your Franchise | Advertise | Restaurant India
+      </p>
+    </div>
+
+  </header>
+  )
+}
+
+export default header
