444f599d573b57789a3ba27b529758a1167a049d
krishpanarah <panarakrish@gmail.com>
2025-09-17 17:36:54 +0530

    gap change

--- src/app/homepage/home.tsx
+++ src/app/homepage/home.tsx
@@ -64,7 +64,7 @@ export default function CarListing() {
       </div>
 
       {/* Car Grid */}
-      <div className="max-w-7xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-6 mx-auto">
+      <div className="max-w-7xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-8 mx-auto">
         {filteredCars.map((car) => (
           <div
             key={car.id}
